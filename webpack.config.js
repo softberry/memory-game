df677dadd2163ba06314579548cffda251c79aff
softberry <emre.sakarya@softberry.de>
2019-01-28 19:40:50 +0100

    set minified filename

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   entry: ['./src/index.js'],
   output: {
-    filename: 'mini-memory.js',
+    filename: 'mini-memory.min.js',
     path: resolve(__dirname, './public'),
     publicPath: '/',
   },
