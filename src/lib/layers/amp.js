068d8526ce3b43afd14b50078040e93ad04de356
softberry <emre.sakarya@softberry.de>
2019-04-10 16:20:57 +0200

    #25 AMP support added

---
+++ src/lib/layers/amp.js
@@ -0,0 +1,28 @@
+/**
+ * @description Prepare AMP Ads Markup for google AdSense.
+ * Required JavaScript Library must be included in
+ * ```<head></head>``` part of publisher document.
+ */
+export class AMP {
+  /**
+   *
+   * @param {{}} options Options object for AMP Custom Element
+   */
+  constructor(options) {
+    this.customElement = options.customElement;
+    this.publisherId = options.publisherId;
+  }
+  /**
+ * @return {String} HtmlMarkup of ad-block if amp info available
+ * in manifest.json, empty otherwise.
+ */
+  markup() {
+    if (!(this.customElement && this.publisherId)) {
+      return '';
+    }
+    return ` <${this.customElement} type="adsense"
+data-ad-client="${this.publisherId}">
+</${this.customElement}>
+`;
+  }
+}
